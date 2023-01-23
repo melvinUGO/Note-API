936df6735e0f232586012a55143db46a1e7e5db8
melvinUGO <ugochukwumelvin@gmail.com>
2023-01-22 19:13:58 -0800

    swagger docs added

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const { json } = require("express");
 const express = require("express");
 const app = express();
@@ -34,8 +39,10 @@ app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.send('<h1>Notes API</H1><a href="/api-docs">Documentation</a>');
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/note", auth, noteRouter);
 app.use(notFoundMiddleware);
